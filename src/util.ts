62a77d567f453adba5f4e28358c8257a5efa5aa8
Joao Moreno <jomo@microsoft.com>
2015-09-18 20:06:13 +0200

    add read util

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
-import { Promise } from 'q';
+import { Promise, nfcall } from 'q';
+import { assign } from 'lodash';
+import _read = require('read');
 
 export function fatal(message: any, ...args: any[]) {
 	if (message instanceof Error && /^cancell?ed$/i.test(message.message)) {
@@ -7,4 +9,9 @@ export function fatal(message: any, ...args: any[]) {
 	
 	console.error('Error:', message, ...args);
 	process.exit(1);
+}
+
+export function read(prompt: string, options: _read.Options = {}): Promise<string> {
+	return nfcall<string>(_read, assign({ prompt }, options))
+		.spread(r => r);
 }
