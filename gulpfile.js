fe5df281b58056c8837f6804b03bd969b44d2734
Joao Moreno <jomo@microsoft.com>
2015-11-17 22:34:50 +0100

    improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ options.sourceRoot = path.join(__dirname, 'src');
 
 var compilation = tsb.create(options);
 
-var compile = function () {
+function compile() {
 	var ts = filter('**/*.ts', { restore: true });
 	var input = es.merge(
 		gulp.src('src/**', { base: 'src', dot: true }),
@@ -24,17 +24,20 @@ var compile = function () {
 		.pipe(compilation())
 		.pipe(ts.restore)
 		.pipe(gulp.dest('out'));
-};
+}
 
 gulp.task('clean', function (cb) { rimraf('out', cb); });
 gulp.task('compile', ['clean'], compile);
-gulp.task('compile-only', compile);
+gulp.task('just-compile', compile);
 
-gulp.task('test', function (cb) {
-	var child = cp.spawn('mocha', ['--reporter=dot'], { stdio: 'inherit' });
+function test(cb) {
+	var child = cp.spawn('mocha', [], { stdio: 'inherit' });
 	child.on('exit', function (code) { cb(); });
-});
+}
 
-gulp.task('watch', ['compile', 'test'], function () {
-	gulp.watch(['src/**', 'typings/**'], ['compile-only', 'test']);
+gulp.task('test', ['compile'], test);
+gulp.task('just-test', ['just-compile'], test);
+
+gulp.task('watch', ['test'], function () {
+	gulp.watch(['src/**', 'typings/**'], ['just-test']);
 });
