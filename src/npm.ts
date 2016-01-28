10daa736552525c31935be273335f8038c0ee8f5
Joao Moreno <jomo@microsoft.com>
2016-01-28 20:06:17 +0100

    rename

--- src/npm.ts
+++ src/npm.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
-import * as child_process from 'child_process';
+import * as cp from 'child_process';
 
 const cmd = 'npm list --production --parseable --depth';
 
 export function getDependencies(cwd: string): Promise<string[]> {
 	return new Promise<string[]>((c, e) => {
-		child_process.exec(cmd, { cwd }, (err, stdout, stderr) => {
+		cp.exec(cmd, { cwd }, (err, stdout, stderr) => {
 				if (err) return e(err);
 
 				c(stdout.toString('utf8')
