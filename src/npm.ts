f39481ab9868e43067fd564368aeaaf319cfc3c6
SrTobi <code.databyte@gmail.com>
2016-02-29 11:32:20 +0100

    added function to determine bad npm versions

--- src/npm.ts
+++ src/npm.ts
@@ -15,3 +15,20 @@ export function getDependencies(cwd: string): Promise<string[]> {
 		);
 	});
 }
+
+const versionCmd = 'npm -v';
+
+const badNpmVersions = [
+	/3.7.[0123]/
+];
+
+export function isBadNpmVersion(): Promise<boolean> {
+	return new Promise<boolean>((c, e) => {
+		cp.exec(versionCmd, (err, stdout, stderr) => {
+			if (err) return e(err);
+			
+			let version = stdout.toString('utf8').trim();
+			c(badNpmVersions.some((regex) => regex.test(version)));
+		});
+	});
+}
