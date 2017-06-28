c54bed54cdc8909b65103ca5abfbbc0afd8370e3
kieferrm <kieferrm@gmail.com>
2017-06-28 15:03:50 -0700

    adding codecov.io as trusted source

--- src/package.ts
+++ src/package.ts
@@ -127,7 +127,8 @@ const TrustedSVGSources = [
 	'badges.frapsoft.com',
 	'cdn.travis-ci.org',
 	'marketplace.visualstudio.com',
-	'ci.appveyor.com'
+	'ci.appveyor.com',
+	'codecov.io'
 ];
 
 function isHostTrusted(host: string): boolean {
