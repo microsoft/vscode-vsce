5ba4c622a1f10114e6742f2cdeff76ad08489d97
João Moreno <mail@joaomoreno.com>
2021-02-03 09:30:26 +0100

    Update src/store.ts

--- src/store.ts
+++ src/store.ts
@@ -64,7 +64,7 @@ export async function verifyPat(pat: string, publisherName?: string): Promise<vo
 			publisherName = (await readManifest()).publisher;
 		} catch (error) {
 			throw new Error(
-				'Can not read the publisher name. Either supply it as argument or call from the package root folder. Additional information:\n\n' +
+				'Can not read the publisher name. Either supply it as argument or run vsce from the extension folder. Additional information:\n\n' +
 					error
 			);
 		}
