0205811f3c70fd4b27e7a1dda8524e64bcacb616
Sandeep Somavarapu <sasomava@microsoft.com>
2021-09-01 12:44:31 +0200

    validate extension kind

--- src/test/package.test.ts
+++ src/test/package.test.ts
@@ -390,6 +390,18 @@ describe('validateManifest', () => {
 		validateManifest(createManifest({ activationEvents: ['any'], browser: 'browser.js' }));
 		validateManifest(createManifest({ activationEvents: ['any'], main: 'main.js', browser: 'browser.js' }));
 	});
+
+	it('should validate extensionKind', () => {
+		assert.throws(() => validateManifest(createManifest({ extensionKind: ['web'] })));
+		assert.throws(() => validateManifest(createManifest({ extensionKind: ['workspace', 'ui', 'web'] })));
+		assert.throws(() => validateManifest(createManifest({ extensionKind: ['workspace', 'web'] })));
+		assert.throws(() => validateManifest(createManifest({ extensionKind: ['ui', 'web'] })));
+		assert.throws(() => validateManifest(createManifest(<any>{ extensionKind: ['any'] })));
+		validateManifest(createManifest({ extensionKind: ['ui'] }));
+		validateManifest(createManifest({ extensionKind: ['workspace'] }));
+		validateManifest(createManifest({ extensionKind: ['ui', 'workspace'] }));
+		validateManifest(createManifest({ extensionKind: ['workspace', 'ui'] }));
+	});
 });
 
 describe('toVsixManifest', () => {
