ab1cb27aed961a242f0a4dd69689cbe1384b8d52
洛竹 <youngjuning@aliyun.com>
2021-09-13 17:54:12 +0800

    chore: add comma

--- src/package.ts
+++ src/package.ts
@@ -251,7 +251,7 @@ const TrustedSVGSources = [
 	'visualstudio.com',
 	'vsmarketplacebadge.apphb.com',
 	'www.bithound.io',
-	'www.versioneye.com'
+	'www.versioneye.com',
 ];
 
 function isGitHubRepository(repository: string | null): boolean {
