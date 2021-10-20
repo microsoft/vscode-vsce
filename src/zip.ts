5ad3393809a91cd3cc10613e85922d0ccb0bc352
João Moreno <joaomoreno@users.noreply.github.com>
2021-10-20 14:49:39 +0200

    fixes #619

--- src/zip.ts
+++ src/zip.ts
@@ -1,70 +1,65 @@
-import { Entry, open, ZipFile } from 'yauzl';
+import { Entry, open, Options, ZipFile } from 'yauzl';
 import { Manifest } from './manifest';
 import { parseXmlManifest, XMLManifest } from './xml';
+import { promisify } from 'util';
+import { Readable } from 'stream';
 
-function readEntryString(zipfile: ZipFile, entry: Entry, cb: (err: Error, result?: string) => void): void {
-	zipfile.openReadStream(entry, (err, stream) => {
-		if (err) {
-			return cb(err);
-		}
-
+async function bufferStream(stream: Readable): Promise<Buffer> {
+	return await new Promise((c, e) => {
 		const buffers = [];
 		stream.on('data', buffer => buffers.push(buffer));
-		stream.once('error', cb);
-		stream.once('end', () => {
-			try {
-				cb(null, Buffer.concat(buffers).toString('utf8'));
-			} catch (err) {
-				cb(err);
+		stream.once('error', e);
+		stream.once('end', () => c(Buffer.concat(buffers)));
+	});
+}
+
+export async function readZip(packagePath: string, filter: (name: string) => boolean): Promise<Map<string, Buffer>> {
+	const zipfile = await promisify<string, Options, ZipFile>(open)(packagePath, { lazyEntries: true });
+
+	return await new Promise((c, e) => {
+		const result = new Map<string, Buffer>();
+
+		zipfile.once('close', () => c(result));
+
+		zipfile.readEntry();
+		zipfile.on('entry', (entry: Entry) => {
+			const name = entry.fileName.toLowerCase();
+
+			if (filter(name)) {
+				zipfile.openReadStream(entry, (err, stream) => {
+					if (err) {
+						zipfile.close();
+						return e(err);
+					}
+
+					bufferStream(stream).then(buffer => {
+						result.set(name, buffer);
+						zipfile.readEntry();
+					});
+				});
+			} else {
+				zipfile.readEntry();
 			}
 		});
 	});
 }
 
-export function readVSIXPackage(packagePath: string): Promise<{ manifest: Manifest; xmlManifest: XMLManifest }> {
-	return new Promise<{ manifest: Manifest; xmlManifest: XMLManifest }>((c, e) => {
-		open(packagePath, (err, zipfile) => {
-			if (err) {
-				return e(err);
-			}
+export async function readVSIXPackage(packagePath: string): Promise<{ manifest: Manifest; xmlManifest: XMLManifest }> {
+	const map = await readZip(packagePath, name => /^extension\/package\.json$|^extension\.vsixmanifest$/i.test(name));
+	const rawManifest = map.get('extension/package.json');
 
-			let manifest: Manifest | undefined;
-			let xmlManifest: XMLManifest | undefined;
+	if (!rawManifest) {
+		throw new Error('Manifest not found');
+	}
 
-			zipfile.once('close', () => {
-				if (!manifest) {
-					e(new Error('Manifest not found'));
-				} else if (!xmlManifest) {
-					e(new Error('VSIX manifest not found'));
-				} else {
-					c({ manifest, xmlManifest });
-				}
-			});
+	const rawXmlManifest = map.get('extension.vsixmanifest');
 
-			zipfile.on('entry', (entry: Entry) => {
-				switch (entry.fileName.toLowerCase()) {
-					case 'extension/package.json': {
-						return readEntryString(zipfile, entry, (err, result) => {
-							if (err) {
-								zipfile.close();
-								return e(err);
-							}
+	if (!rawXmlManifest) {
+		throw new Error('VSIX manifest not found');
+	}
 
-							manifest = JSON.parse(result);
-						});
-					}
-					case 'extension.vsixmanifest': {
-						return readEntryString(zipfile, entry, (err, result) => {
-							if (err) {
-								zipfile.close();
-								return e(err);
-							}
-
-							parseXmlManifest(result).then(r => (xmlManifest = r));
-						});
-					}
-				}
-			});
-		});
-	});
+	return {
+		manifest: JSON.parse(rawManifest.toString('utf8')),
+		xmlManifest: await parseXmlManifest(rawXmlManifest.toString('utf8')),
+	};
 }
