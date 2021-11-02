12e787dc28f919756bb71f6785adc19a52dda0ec
João Moreno <joaomoreno@users.noreply.github.com>
2021-11-02 15:54:08 +0100

    test: add FileStore iterator tests

--- src/test/store.test.ts
+++ src/test/store.test.ts
@@ -9,16 +9,19 @@ describe('FileStore', () => {
 		const store = await FileStore.open(name);
 
 		assert.deepStrictEqual(store.get('joe'), undefined);
+		assert.deepStrictEqual([...store], []);
 		assert.ok(!fs.existsSync(name));
 
 		await store.add({ name: 'joe', pat: 'abc' });
 		assert.deepStrictEqual(store.get('joe'), { name: 'joe', pat: 'abc' });
+		assert.deepStrictEqual([...store], [{ name: 'joe', pat: 'abc' }]);
 		assert.deepStrictEqual(JSON.parse(fs.readFileSync(name, 'utf8')), {
 			publishers: [{ name: 'joe', pat: 'abc' }],
 		});
 
 		await store.add({ name: 'joe', pat: 'what' });
 		assert.deepStrictEqual(store.get('joe'), { name: 'joe', pat: 'what' });
+		assert.deepStrictEqual([...store], [{ name: 'joe', pat: 'what' }]);
 		assert.deepStrictEqual(JSON.parse(fs.readFileSync(name, 'utf8')), {
 			publishers: [{ name: 'joe', pat: 'what' }],
 		});
@@ -26,6 +29,13 @@ describe('FileStore', () => {
 		await store.add({ name: 'jane', pat: 'oh' });
 		assert.deepStrictEqual(store.get('joe'), { name: 'joe', pat: 'what' });
 		assert.deepStrictEqual(store.get('jane'), { name: 'jane', pat: 'oh' });
+		assert.deepStrictEqual(
+			[...store],
+			[
+				{ name: 'joe', pat: 'what' },
+				{ name: 'jane', pat: 'oh' },
+			]
+		);
 		assert.deepStrictEqual(JSON.parse(fs.readFileSync(name, 'utf8')), {
 			publishers: [
 				{ name: 'joe', pat: 'what' },
@@ -36,6 +46,7 @@ describe('FileStore', () => {
 		await store.delete('joe');
 		assert.deepStrictEqual(store.get('joe'), undefined);
 		assert.deepStrictEqual(store.get('jane'), { name: 'jane', pat: 'oh' });
+		assert.deepStrictEqual([...store], [{ name: 'jane', pat: 'oh' }]);
 		assert.deepStrictEqual(JSON.parse(fs.readFileSync(name, 'utf8')), {
 			publishers: [{ name: 'jane', pat: 'oh' }],
 		});
@@ -43,6 +54,7 @@ describe('FileStore', () => {
 		await store.delete('joe');
 		assert.deepStrictEqual(store.get('joe'), undefined);
 		assert.deepStrictEqual(store.get('jane'), { name: 'jane', pat: 'oh' });
+		assert.deepStrictEqual([...store], [{ name: 'jane', pat: 'oh' }]);
 		assert.deepStrictEqual(JSON.parse(fs.readFileSync(name, 'utf8')), {
 			publishers: [{ name: 'jane', pat: 'oh' }],
 		});
@@ -50,6 +62,7 @@ describe('FileStore', () => {
 		await store.delete('jane');
 		assert.deepStrictEqual(store.get('joe'), undefined);
 		assert.deepStrictEqual(store.get('jane'), undefined);
+		assert.deepStrictEqual([...store], []);
 		assert.deepStrictEqual(JSON.parse(fs.readFileSync(name, 'utf8')), {
 			publishers: [],
 		});
