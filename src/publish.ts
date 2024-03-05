e7abedadd996e9ec91eec209aa642bc956f23f13
Sandeep Somavarapu <sasomava@microsoft.com>
2024-03-05 10:43:04 +0100

    add extra linebreak required for .net servers

--- src/publish.ts
+++ src/publish.ts
@@ -259,10 +259,10 @@ async function _publishSignedPackage(api: GalleryApi, packageName: string, packa
 	const lineBreak = '\r\n';
 	form.setBoundary('0f411892-ef48-488f-89d3-4f0546e84723');
 	form.append('vsix', packageStream, {
-		header: `--${form.getBoundary()}${lineBreak}Content-Disposition: attachment; name=vsix; filename=${packageName}${lineBreak}Content-Type: application/octet-stream${lineBreak}`
+		header: `--${form.getBoundary()}${lineBreak}Content-Disposition: attachment; name=vsix; filename=${packageName}${lineBreak}Content-Type: application/octet-stream${lineBreak}${lineBreak}`
 	});
 	form.append('sigzip', sigzipStream, {
-		header: `--${form.getBoundary()}${lineBreak}Content-Disposition: attachment; name=sigzip; filename=${sigzipName}${lineBreak}Content-Type: application/octet-stream${lineBreak}`
+		header: `--${form.getBoundary()}${lineBreak}Content-Disposition: attachment; name=sigzip; filename=${sigzipName}${lineBreak}Content-Type: application/octet-stream${lineBreak}${lineBreak}`
 	});
 	requestOptions.additionalHeaders = {
 		...form.getHeaders(),
