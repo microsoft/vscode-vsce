abe171c64694ffdbf970229d26a459b1a34921be
BeniBenj <besimmonds@microsoft.com>
2024-04-11 17:25:58 +0200

    change test

--- src/test/validation.test.ts
+++ src/test/validation.test.ts
@@ -72,7 +72,7 @@ describe('validateVersion', () => {
 		assert.throws(() => validateVersion('.0.1'));
 		assert.throws(() => validateVersion('0.1.'));
 		assert.throws(() => validateVersion('0.0.0.1'));
-		assert.throws(() => validateVersion('1.0.a'));
+		assert.throws(() => validateVersion('1.0-pre'));
 	});
 });
 
