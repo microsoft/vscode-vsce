70f353522aa037a95353b748601993a06cd659a9
Joyce Er <joyceerhl@gmail.com>
2025-09-25 04:01:48 -0700

    fix: generate language model tag for `languageModelChatProvider` contributions

--- src/package.ts
+++ src/package.ts
@@ -683,7 +683,7 @@ export class TagsProcessor extends BaseProcessor {
 		const remoteMenu = doesContribute('menus', 'statusBar/remoteIndicator') ? ['remote-menu'] : [];
 		const chatParticipants = doesContribute('chatParticipants') ? ['chat-participant'] : [];
 		const languageModelTools = doesContribute('languageModelTools') ? ['tools', 'language-model-tools'] : [];
-		const languageModels = doesContribute('languageModels') ? ['language-models'] : [];
+		const languageModels = doesContribute('languageModelChatProviders') ? ['language-models'] : [];
 		const mcp = doesContribute('modelContextServerCollections') ? ['mcp', 'language-model-tools'] : [];
 
 		const localizationContributions = ((contributes && contributes['localizations']) ?? []).reduce<string[]>(
