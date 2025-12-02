893003bca36b0c4f2a027295795506a22280852d
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-12-02 17:51:23 +0100

    style: classes

--- src/managers/manager.ts
+++ src/managers/manager.ts
@@ -12,48 +12,48 @@ export type PackageManagerLiteral = typeof managers[number];
  * Interface for package manager implementations.
  * Using interface to force explicit implementations.
  */
-export interface IPackageManager {
+export abstract class PackageManager {
 	/**
 	 * The binary name of the package manager.
 	 * @example 'yarn' | 'npm' | 'bun'
 	 */
-	binaryName: string;
+	abstract binaryName: string;
 
 	/**
 	 * Get the version of the package manager itself.
 	 */
-	selfVersion(cancellationToken?: CancellationToken): Promise<string>;
+	abstract selfVersion(cancellationToken?: CancellationToken): Promise<string>;
 
 	/**
 	 * Check if the package manager version and configs are compatible.
 	 */
-	selfCheck(cancellationToken?: CancellationToken): Promise<void>;
+	abstract selfCheck(cancellationToken?: CancellationToken): Promise<void>;
 
 	/**
 	 * Get the command to run a script.
 	 */
-	commandRun(scriptName: string): string;
+	abstract commandRun(scriptName: string): string;
 
 	/**
 	 * Get the command to install a package.
 	 */
-	commandInstall(packageName: string, global: boolean): string;
+	abstract commandInstall(packageName: string, global: boolean): string;
 
 	/**
 	 * Request the latest version of a package from the registry.
 	 */
-	pkgRequestLatest(name: string, cancellationToken?: CancellationToken): Promise<string>;
+	abstract pkgRequestLatest(name: string, cancellationToken?: CancellationToken): Promise<string>;
 
 	/**
 	 * Get the production dependencies of a package.
 	 */
-	pkgProdDependencies(cwd: string, packagedDependencies?: string[]): Promise<string[]>;
+	abstract pkgProdDependencies(cwd: string, packagedDependencies?: string[]): Promise<string[]>;
 
 	/**
 	 * Get the files of production dependencies of a package.
 	 * Should use pkgProdDependencies first to get the dependencies.
 	 */
-	pkgProdDependenciesFiles(cwd: string, deps: string[], followSymlinks?: boolean): Promise<string[]>;
+	abstract pkgProdDependenciesFiles(cwd: string, deps: string[], followSymlinks?: boolean): Promise<string[]>;
 }
 
 /**
@@ -62,12 +62,12 @@ export interface IPackageManager {
  */
 export function getPackageManager(
 	preference: PackageManagerLiteral = "npm",
-): IPackageManager {
+): PackageManager {
 	const choice = {
 		"none": pmNone,
 		"npm": pmNPM,
 		"yarn": pmYarn,
-	} as Record<PackageManagerLiteral, IPackageManager>
+	} as Record<PackageManagerLiteral, PackageManager>
 
 	return choice[preference]
 }
--- src/managers/npm.ts
+++ src/managers/npm.ts
@@ -2,42 +2,42 @@ import * as path from 'path';
 import * as semver from 'semver';
 import { glob } from 'glob';
 import { exec } from "./exec";
-import type { IPackageManager } from "./manager";
+import { PackageManager } from "./manager";
 import type { CancellationToken } from "../util";
 
-export const pmNPM: IPackageManager = {
-	binaryName: 'npm',
+export class PackageManagerNpm extends PackageManager {
+	binaryName = 'npm'
 
 	async selfVersion(cancellationToken?: CancellationToken): Promise<string> {
 		const { stdout } = await exec('npm -v', {}, cancellationToken);
 		return stdout.trim();
-	},
+	}
 	async selfCheck(cancellationToken?: CancellationToken): Promise<void> {
 		const version = await this.selfVersion(cancellationToken);
 		if (semver.intersects(version, '< 6')) {
 			throw new Error(`npm@${version} doesn't work with vsce. Please update npm: ${this.commandInstall('npm', true)}`);
 		}
-	},
+	}
 
 	commandRun(scriptName: string): string {
 		return `${this.binaryName} run ${scriptName}`
-	},
+	}
 	commandInstall(pkg: string, global: boolean): string {
 		let flag = (global ? '-g' : '')
 		flag &&= flag + " "
 		return `${this.binaryName} install ${flag}${pkg}`
-	},
+	}
 
 	async pkgRequestLatest(name: string, cancellationToken?: CancellationToken): Promise<string> {
 		await this.selfCheck(cancellationToken)
 		const { stdout } = await exec(`npm show ${name} version`, {}, cancellationToken)
 		return stdout.split(/[\r\n]/).filter(line => !!line)[0];
-	},
+	}
 	async pkgProdDependencies(cwd: string, _?: string[]): Promise<string[]> {
 		await this.selfCheck()
 		const { stdout } = await exec('npm list --production --parseable --depth=99999 --loglevel=error', { cwd, maxBuffer: 5000 * 1024 })
 		return stdout.split(/[\r\n]/).filter(dir => path.isAbsolute(dir))
-	},
+	}
 	async pkgProdDependenciesFiles(cwd: string, deps: string[], followSymlinks?: boolean): Promise<string[]> {
 		const promises = deps.map(dep =>
 			glob('**', { cwd: dep, nodir: true, follow: followSymlinks, dot: true, ignore: 'node_modules/**' }).then(files =>
@@ -47,5 +47,7 @@ export const pmNPM: IPackageManager = {
 
 		const files = (await Promise.all(promises)).flat()
 		return files;
-	},
+	}
 }
+
+export const pmNPM = new PackageManagerNpm()
